0a6dc54847000ba158d06fe4072d0ebfea455931
Saajid Khan <simbak313@gmail.com>
2022-03-22 23:39:53 -0400

    updated copy

--- app/mail.php
+++ app/mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-         $mail_to = "Put your email here";
+         $mail_to = "saajid718@gmail.com";
          
         # Sender Data
         $subject = trim($_POST["subject"]);
